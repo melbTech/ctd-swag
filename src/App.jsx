99480664f886795dd350b53f63f761fbd6a2afb6
Melvin Santos <103694007+melbTech@users.noreply.github.com>
2025-11-24 12:38:40 -0500

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ctdLogo from './assets/icons/mono-blue-logo.svg';
+import './App.css';
+import { Center } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="coming-soon">
+      <h1>CTD Swag</h1>
+      <div style={{ height: 100, width: 100 }}>
+        <img src={ctdLogo} alt="Code the Dream Logo" />
+      </div>
+      <h2>Coming Soon</h2>
+    </div>
+  );
+}
+
+export default App;
