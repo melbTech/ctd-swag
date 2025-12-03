f4e979205c0f7589bdb80612cffb9bab93491a51
Melvin Santos <103694007+melbTech@users.noreply.github.com>
2025-12-02 20:34:29 -0500

    create inventory from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import ctdLogo from './assets/icons/mono-blue-logo.svg';
 import './App.css';
-import { Center } from '@chakra-ui/react';
+import InventoryData from './assets/inventory.json';
 
 function App() {
+  const [inventory, setInventory] = useState(InventoryData.inventory);
   return (
-    <div className="coming-soon">
-      <h1>CTD Swag</h1>
-      <div style={{ height: 100, width: 100 }}>
-        <img src={ctdLogo} alt="Code the Dream Logo" />
+    <main>
+      <div className="coming-soon">
+        <h1>CTD Swag</h1>
+        <div style={{ height: 100, width: 100 }}>
+          <img src={ctdLogo} alt="Code the Dream Logo" />
+        </div>
       </div>
-      <h2>Coming Soon</h2>
-    </div>
+      <ul>
+        {inventory.map((item) => {
+          return (
+            <li key={item.id}>
+              <div className="itemCard">
+                <h2>{item.baseName}</h2>
+                <p>{item.baseDescription}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
 
